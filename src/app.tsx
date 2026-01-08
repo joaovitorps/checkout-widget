ab6ca7d6f7fc8097db33ede1b791f7f74c042347
João Santos <dev.joaosantos@outlook.com>
2026-01-08 14:14:03 -0300

    fix: move function outside component's body so it does not get created again when component re-renders

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,13 @@ interface CmsDataProps {
   currency: string;
 }
 
-export function App({ price, currency }: CmsDataProps) {
-  function formattedCurrency({ price, currency }: CmsDataProps) {
-    return new Intl.NumberFormat("en-GB", {
-      style: "currency",
-      currency: currency,
-    }).format(price / 100);
-  }
+function formattedCurrency({ price, currency }: CmsDataProps) {
+  return new Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: currency,
+  }).format(price / 100);
+}
 
+export function App({ price, currency }: CmsDataProps) {
   return <button>Pay {formattedCurrency({ price, currency })}</button>;
 }
