cb9225c95f18ff60b08acfd297a3553cfe726bd6
João Santos <dev.joaosantos@outlook.com>
2026-01-08 11:23:12 -0300

    feat: add function to format currency based on `CMS` data along with a button

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,15 @@
-export function App() {
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  );
+interface CmsDataProps {
+  price: number;
+  currency: string;
+}
+
+export function App({ price, currency }: CmsDataProps) {
+  function formattedCurrency({ price, currency }: CmsDataProps) {
+    return new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: currency,
+    }).format(price / 100);
+  }
+
+  return <button>Pay {formattedCurrency({ price, currency })}</button>;
 }
