3b5d420f5591c63edf78069c72ac4537c39c45f3
João Santos <dev.joaosantos@outlook.com>
2026-01-08 11:58:34 -0300

    fix: wrong parse and default values

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,8 @@ import { App } from "./app.tsx";
 try {
   const element = document.getElementById("plugin-container");
 
-  let price: unknown = element?.dataset.price;
-  let priceNumber: number = price as number;
-
-  if (!element?.dataset.currency) {
-    throw new Error("Currency not found");
-  }
-
-  let currency: string = element?.dataset.currency;
-
-  console.log(element?.dataset);
+  let priceNumber: number = parseInt(element?.dataset.price || "0", 10);
+  let currency: string = element?.dataset.currency || "EUR";
 
   createRoot(element!).render(
     <StrictMode>
