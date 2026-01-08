bea6fddfd3f4478f02b89b48ad1e94884969b801
João Santos <dev.joaosantos@outlook.com>
2026-01-08 11:22:45 -0300

    feat: send `CMS` data through props to my `App` component

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+try {
+  const element = document.getElementById("plugin-container");
+
+  let price: unknown = element?.dataset.price;
+  let priceNumber: number = price as number;
+
+  if (!element?.dataset.currency) {
+    throw new Error("Currency not found");
+  }
+
+  let currency: string = element?.dataset.currency;
+
+  console.log(element?.dataset);
+
+  createRoot(element!).render(
+    <StrictMode>
+      <App price={priceNumber} currency={currency} />
+    </StrictMode>
+  );
+} catch (error) {
+  console.error("checkout-widget-error:", error);
+}
