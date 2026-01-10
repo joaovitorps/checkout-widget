8ac4f30095fe4cceb7bea43231df9dfb3c15dcb3
João Santos <dev.joaosantos@outlook.com>
2026-01-10 13:00:29 -0300

    test: add test cases for handling payment method fail and success

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,7 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import { render, fireEvent, screen } from "@testing-library/react";
 import { App } from "./app";
+import * as api from "./api";
 
 test("renders Button with price and currency", async () => {
   render(<App price={5500} currency="EUR" />);
@@ -24,4 +25,43 @@ test("show loading message when clicked", async () => {
   fireEvent.click(paymentButton);
   expect(paymentButton).toHaveTextContent(/^Loading...$/);
   expect(paymentButton).toBeDisabled();
+});
+
+test("check result payment success", async () => {
+  const spy = vi
+    .spyOn(api, "apiCall")
+    .mockImplementation(() => Promise.resolve(1));
+
+  render(<App price={5500} currency="EUR" />);
+  const paymentButton = screen.getByRole("button", {
+    name: "Pay €55.00",
+  });
+
+  fireEvent.click(paymentButton);
+
+  expect(spy).toHaveBeenCalled();
+});
+
+test("check result payment fail", async () => {
+  const spy = vi
+    .spyOn(api, "apiCall")
+    .mockRejectedValue(new Error("Blocked by test"));
+
+  render(<App price={5500} currency="EUR" />);
+  const paymentButton = screen.getByRole("button", {
+    name: "Pay €55.00",
+  });
+
+  fireEvent.click(paymentButton);
+
+  expect(spy).toHaveBeenCalled();
+
+  const spanError = await screen.findByRole("alert");
+
+  expect(spanError).toHaveTextContent(
+    "We were unable to fulfill the payment now, please try again in a few minutes"
+  );
+  expect(spanError).toAppearAfter(paymentButton);
+
+  expect(paymentButton).not.toBeDisabled();
 });
